0346036b3e547f8c8c5985e4bb3aea1642588c2b
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2023-06-12 19:32:02 +0300

    Removed magic numbers from ui.py

--- cell.py
+++ cell.py
@@ -3,11 +3,14 @@ from tkinter import Button
 
 class Cell:
     def revealCell(self, colour_pallette):
-        self.button.config(text=self.value, fg=colour_pallette[str(self.value)], relief='sunken')
+        self.button.config(text=self.value, fg=colour_pallette[str(self.value)], relief=self.PRESSED_RELIEF)
 
 
     def __init__(self, x, y, value, parent):
+        self.PRESSED_RELIEF = 'sunken'
+        self.BUTTON_WIDTH = 2
+        self.BACKGROUND_COLOR = '#CACACA'
         self.x = x
         self.y = y
         self.value = value
-        self.button = Button(width=2, bg='#CACACA', command=lambda: parent.updateBoard(self.x, self.y))
+        self.button = Button(width=self.BUTTON_WIDTH, bg=self.BACKGROUND_COLOR, command=lambda: parent.updateBoard(self.x, self.y))
